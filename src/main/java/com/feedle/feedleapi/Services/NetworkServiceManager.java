9dd3469287e319582a64fdc25101262698385315
Eugene4JD <36538202+Eugene4JD@users.noreply.github.com>
2020-12-07 16:38:20 +0100

    fv

--- src/main/java/com/feedle/feedleapi/Services/NetworkServiceManager.java
+++ src/main/java/com/feedle/feedleapi/Services/NetworkServiceManager.java
@@ -6,6 +6,9 @@ import com.feedle.feedleapi.Models.Post;
 import com.feedle.feedleapi.Models.User;
 import com.feedle.feedleapi.Networking.*;
 import com.feedle.feedleapi.Services.NetworkService;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.stream.JsonReader;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
@@ -19,6 +22,7 @@ public class NetworkServiceManager implements NetworkService {
     private DataInputStream in;
     private DataOutputStream out;
     private ObjectMapper mapper = new ObjectMapper();
+    private Gson gson = new Gson();
 
     private static int PORT = 5000;
     private static String HOST = "localHost";
@@ -33,13 +37,15 @@ public class NetworkServiceManager implements NetworkService {
     public Post addPost(Post post) {
         try {
             AddPostRequest addPostRequest = new AddPostRequest(post);
-            String requestAsJson = mapper.writeValueAsString(addPostRequest);
+            //String requestAsJson = mapper.writeValueAsString(addPostRequest);
+            String requestAsJson = gson.toJson(addPostRequest);
             out.write(requestAsJson.getBytes());
             System.out.println("AddPostRequestSent");
             byte[] response = in.readAllBytes();
             String responseAsJson = new String(response);
             System.out.println(responseAsJson);
-            AddPostRequest addPostResponse = mapper.convertValue(requestAsJson, AddPostRequest.class);
+           // AddPostRequest addPostResponse = mapper.convertValue(requestAsJson, AddPostRequest.class);
+            AddPostRequest addPostResponse = gson.fromJson(requestAsJson, AddPostRequest.class);
             return addPostResponse.getPost();
         } catch (Exception e) {
             e.printStackTrace();
@@ -51,12 +57,14 @@ public class NetworkServiceManager implements NetworkService {
     public User addUser(User user) {
         try {
             PostUserRequest postUserRequest = new PostUserRequest(user);
-            String requestAsJson = mapper.writeValueAsString(postUserRequest);
+            // String requestAsJson = mapper.writeValueAsString(postUserRequest);
+            String requestAsJson = gson.toJson(postUserRequest);
             out.write(requestAsJson.getBytes());
             System.out.println("AddUserRequestSent");
             byte[] response = in.readAllBytes();
             String responseAsJson = new String(response);
-            PostUserRequest postUserResponse = mapper.convertValue(requestAsJson, PostUserRequest.class);
+            //PostUserRequest postUserResponse = mapper.convertValue(requestAsJson, PostUserRequest.class);
+            PostUserRequest postUserResponse = gson.fromJson(requestAsJson, PostUserRequest.class);
             return postUserResponse.getUser();
         } catch (Exception e) {
             e.printStackTrace();
@@ -68,13 +76,14 @@ public class NetworkServiceManager implements NetworkService {
     public List<User> getAllUser() {
         try {
             GetUsersRequest getUsersRequest = new GetUsersRequest();
-            String requestAsJson = mapper.writeValueAsString(getUsersRequest);
+            //String requestAsJson = mapper.writeValueAsString(getUsersRequest);
+            String requestAsJson = gson.toJson(getUsersRequest);
             byte[] messageInBytes = requestAsJson.getBytes();
             out.write(messageInBytes);
             System.out.println("GetUsersRequestSent");
-            byte[] response = in.readAllBytes();
-            String responseAsJson = new String(response);
-            GetUsersResponse getUsersResponse = mapper.convertValue(responseAsJson, GetUsersResponse.class);
+            byte[] response = new byte[1024];
+            in.read(response,0,response.length);
+            GetUsersResponse getUsersResponse = gson.fromJson(parseJson(response), GetUsersResponse.class);
             return getUsersResponse.getUsers();
         } catch (Exception e) {
             e.printStackTrace();
@@ -86,13 +95,13 @@ public class NetworkServiceManager implements NetworkService {
     public List<Post> getAllPost() {
         try {
             GetPostsRequest getPostsRequest = new GetPostsRequest();
-            String requestAsJson = mapper.writeValueAsString(getPostsRequest);
+            String requestAsJson = gson.toJson(getPostsRequest);
             byte[] messageInBytes = requestAsJson.getBytes();
             out.write(messageInBytes);
             System.out.println("GetPostsRequestSent");
-            byte[] response = in.readAllBytes();
-            String responseAsJson = new String(response);
-            GetPostsResponse getPostsResponse = mapper.convertValue(responseAsJson, GetPostsResponse.class);
+            byte[] response = new byte[1024];
+            in.read(response,0,response.length);
+            GetPostsResponse getPostsResponse = gson.fromJson(parseJson(response), GetPostsResponse.class);
             return getPostsResponse.getPostList();
         } catch (Exception e) {
             e.printStackTrace();
@@ -104,14 +113,16 @@ public class NetworkServiceManager implements NetworkService {
     public Post updatePost(Post post) {
         try {
             UpdatePostRequest updatePostRequest = new UpdatePostRequest(post);
-            String requestAsJson = mapper.writeValueAsString(updatePostRequest);
+           // String requestAsJson = mapper.writeValueAsString(updatePostRequest);
+            String requestAsJson = gson.toJson(updatePostRequest);
             byte[] messageInBytes = requestAsJson.getBytes();
             out.write(messageInBytes);
             System.out.println("UpdatePostRequestSent");
             byte[] response = in.readAllBytes();
             String responseAsJson = new String(response);
-            UpdatePostRequest updatePostResponse = mapper.convertValue(responseAsJson, UpdatePostRequest.class);
-            return updatePostRequest.getPost();
+            //UpdatePostRequest updatePostResponse = mapper.convertValue(responseAsJson, UpdatePostRequest.class);
+            UpdatePostRequest updatePostResponse = gson.fromJson(responseAsJson, UpdatePostRequest.class);
+            return updatePostResponse.getPost();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -122,13 +133,15 @@ public class NetworkServiceManager implements NetworkService {
     public User updateUser(User user) {
         try {
             UpdateUserRequest updateUserRequest = new UpdateUserRequest(user);
-            String requestAsJson = mapper.writeValueAsString(updateUserRequest);
+           // String requestAsJson = mapper.writeValueAsString(updateUserRequest);
+            String requestAsJson = gson.toJson(updateUserRequest);
             byte[] messageInBytes = requestAsJson.getBytes();
             out.write(messageInBytes);
             System.out.println("UpdateUserRequestSent");
             byte[] response = in.readAllBytes();
             String responseAsJson = new String(response);
-            UpdateUserRequest updateUserResponse = mapper.convertValue(responseAsJson, UpdateUserRequest.class);
+            //UpdateUserRequest updateUserResponse = mapper.convertValue(responseAsJson, UpdateUserRequest.class);
+            UpdateUserRequest updateUserResponse = gson.fromJson(responseAsJson, UpdateUserRequest.class);
             return updateUserResponse.getUser();
         } catch (Exception e) {
             e.printStackTrace();
@@ -140,13 +153,15 @@ public class NetworkServiceManager implements NetworkService {
     public int deleteUser(int userId) {
         try {
             DeleteUserRequest deleteUserRequest = new DeleteUserRequest(userId);
-            String requestAsJson = mapper.writeValueAsString(deleteUserRequest);
+            //String requestAsJson = mapper.writeValueAsString(deleteUserRequest);
+            String requestAsJson = gson.toJson(deleteUserRequest);
             byte[] messageInBytes = requestAsJson.getBytes();
             out.write(messageInBytes);
             System.out.println("DeleteUserRequest");
             byte[] response = in.readAllBytes();
             String responseAsJson = new String(response);
-            DeleteUserRequest deleteUserResponse = mapper.convertValue(responseAsJson, DeleteUserRequest.class);
+            //DeleteUserRequest deleteUserResponse = mapper.convertValue(responseAsJson, DeleteUserRequest.class);
+            DeleteUserRequest deleteUserResponse = gson.fromJson(responseAsJson, DeleteUserRequest.class);
             return deleteUserResponse.getUserId();
         } catch (Exception e) {
             e.printStackTrace();
@@ -159,17 +174,26 @@ public class NetworkServiceManager implements NetworkService {
     public int deletePost(int postId) {
         try {
             DeletePostRequest deletePostRequest = new DeletePostRequest(postId);
-            String requestAsJson = mapper.writeValueAsString(deletePostRequest);
+            //String requestAsJson = mapper.writeValueAsString(deletePostRequest);
+            String requestAsJson = gson.toJson(deletePostRequest);
             byte[] messageInBytes = requestAsJson.getBytes();
             out.write(messageInBytes);
             System.out.println("DeletePostRequest");
             byte[] response = in.readAllBytes();
             String responseAsJson = new String(response);
-            DeletePostRequest deletePostResponse = mapper.convertValue(responseAsJson, DeletePostRequest.class);
+            //DeletePostRequest deletePostResponse = mapper.convertValue(responseAsJson, DeletePostRequest.class);
+            DeletePostRequest deletePostResponse = gson.fromJson(responseAsJson, DeletePostRequest.class);
             return deletePostResponse.getPostId();
         } catch (Exception e) {
             e.printStackTrace();
         }
         return -1;
+    }
+
+    private JsonReader parseJson(byte[] bytes)
+    {
+        JsonReader reader = new JsonReader(new StringReader(new String(bytes)));
+        reader.setLenient(true);
+        return reader;
     }
 }
